0cd771db7adefaf7312becb9792c3d5d9c2193cc
Ignas Grigaravicius <kickeris@gmail.com>
2017-09-12 22:25:30 +0100

    Styling css added

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -13,7 +13,7 @@ private albumsUrl: string;
   }
 searchMusic(str: string, type = 'artist') {
   let headers = new Headers();
-  let authToken = '';
+  let authToken = 'BQAvUery4C9ecoC8hUoDiyFJyDA9kOsg9OKSybeNrcITOL0kkLyTW5NQ2oEaKkJL8JZ53URspR2L2ol83JZ6PzcB5CTmo43MSf2mGmLmmRR83-b8chdQEViPyYw22k0tRcuv';
   headers.append('Authorization', 'Bearer '+authToken);
   		this.searchUrl = "https://api.spotify.com/v1/search?query="
   			+ str + "&offset=0&limit=20&type="
@@ -23,7 +23,7 @@ searchMusic(str: string, type = 'artist') {
   	}
  getArtist(id: string) {
   let headers = new Headers();
-  let authToken = '';
+  let authToken = 'BQAvUery4C9ecoC8hUoDiyFJyDA9kOsg9OKSybeNrcITOL0kkLyTW5NQ2oEaKkJL8JZ53URspR2L2ol83JZ6PzcB5CTmo43MSf2mGmLmmRR83-b8chdQEViPyYw22k0tRcuv';
   headers.append('Authorization', 'Bearer '+authToken);
   		this.artistUrl = "https://api.spotify.com/v1/artists/" + id;
   return this._http.get(this.artistUrl, { headers }).map(res => res.json());
@@ -32,7 +32,7 @@ searchMusic(str: string, type = 'artist') {
 
  getAlbums(artistId: string) {
 let headers = new Headers();
-let authToken = '';
+let authToken = 'BQAvUery4C9ecoC8hUoDiyFJyDA9kOsg9OKSybeNrcITOL0kkLyTW5NQ2oEaKkJL8JZ53URspR2L2ol83JZ6PzcB5CTmo43MSf2mGmLmmRR83-b8chdQEViPyYw22k0tRcuv';
 headers.append('Authorization', 'Bearer '+authToken);
   		this.albumsUrl = "https://api.spotify.com/v1/artists/" + artistId + "/albums";
   return this._http.get(this.albumsUrl, { headers }).map(res => res.json());
