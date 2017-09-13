d2010b6279113c289058605c5be07d714199ca5c
Ignas Grigaravicius <kickeris@gmail.com>
2017-09-13 08:13:43 +0100

    Albums added to components, routing, musicService updated,

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -8,12 +8,13 @@ export class MusicService {
 private searchUrl:string;
 private artistUrl: string;
 private albumsUrl: string;
+private albumUrl:string;
 
   constructor(private _http:Http) { 
   }
 searchMusic(str: string, type = 'artist') {
   let headers = new Headers();
-  let authToken = 'BQAvUery4C9ecoC8hUoDiyFJyDA9kOsg9OKSybeNrcITOL0kkLyTW5NQ2oEaKkJL8JZ53URspR2L2ol83JZ6PzcB5CTmo43MSf2mGmLmmRR83-b8chdQEViPyYw22k0tRcuv';
+  let authToken = 'BQAih1siO3ka3ed9Ml_YlItX8_vJ_aBfebknaGoEdimlUh-efjwmhux1cZx_2BldD1vBA4MDVFB1dJ4LCE3ouy37qc609SgyxnZah_aV70a4pGT3hmmf5yZcTscv2EAkXY5L';
   headers.append('Authorization', 'Bearer '+authToken);
   		this.searchUrl = "https://api.spotify.com/v1/search?query="
   			+ str + "&offset=0&limit=20&type="
@@ -23,7 +24,7 @@ searchMusic(str: string, type = 'artist') {
   	}
  getArtist(id: string) {
   let headers = new Headers();
-  let authToken = 'BQAvUery4C9ecoC8hUoDiyFJyDA9kOsg9OKSybeNrcITOL0kkLyTW5NQ2oEaKkJL8JZ53URspR2L2ol83JZ6PzcB5CTmo43MSf2mGmLmmRR83-b8chdQEViPyYw22k0tRcuv';
+  let authToken = 'BQAih1siO3ka3ed9Ml_YlItX8_vJ_aBfebknaGoEdimlUh-efjwmhux1cZx_2BldD1vBA4MDVFB1dJ4LCE3ouy37qc609SgyxnZah_aV70a4pGT3hmmf5yZcTscv2EAkXY5L';
   headers.append('Authorization', 'Bearer '+authToken);
   		this.artistUrl = "https://api.spotify.com/v1/artists/" + id;
   return this._http.get(this.artistUrl, { headers }).map(res => res.json());
@@ -32,9 +33,17 @@ searchMusic(str: string, type = 'artist') {
 
  getAlbums(artistId: string) {
 let headers = new Headers();
-let authToken = 'BQAvUery4C9ecoC8hUoDiyFJyDA9kOsg9OKSybeNrcITOL0kkLyTW5NQ2oEaKkJL8JZ53URspR2L2ol83JZ6PzcB5CTmo43MSf2mGmLmmRR83-b8chdQEViPyYw22k0tRcuv';
+let authToken = 'BQAih1siO3ka3ed9Ml_YlItX8_vJ_aBfebknaGoEdimlUh-efjwmhux1cZx_2BldD1vBA4MDVFB1dJ4LCE3ouy37qc609SgyxnZah_aV70a4pGT3hmmf5yZcTscv2EAkXY5L';
 headers.append('Authorization', 'Bearer '+authToken);
   		this.albumsUrl = "https://api.spotify.com/v1/artists/" + artistId + "/albums";
   return this._http.get(this.albumsUrl, { headers }).map(res => res.json());
+  }
+
+ getAlbum(id: string) {
+let headers = new Headers();
+let authToken = 'BQAih1siO3ka3ed9Ml_YlItX8_vJ_aBfebknaGoEdimlUh-efjwmhux1cZx_2BldD1vBA4MDVFB1dJ4LCE3ouy37qc609SgyxnZah_aV70a4pGT3hmmf5yZcTscv2EAkXY5L';
+headers.append('Authorization', 'Bearer '+authToken);
+  		this.albumUrl = "https://api.spotify.com/v1/albums/" + id;
+  return this._http.get(this.albumUrl, { headers }).map(res => res.json());
   	}
 }
